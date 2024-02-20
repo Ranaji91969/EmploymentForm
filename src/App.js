b3543d3cfef034966e30f38e98e9aee1ea516032
Ranaji91969 <159106117+Ranaji91969@users.noreply.github.com>
2024-02-20 10:07:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import EmploymentSatisfactionForm from './app/EmployeeSatisfactionForm';
+import "../src/styles/satisfaction/AdditionalComments.css"
+import "../src/styles/satisfaction/PersonalInfo.css"
+import "../src/styles/satisfaction/TeamMatesHelpfulness.css"
+import "../src/styles/satisfaction/SatisfactionRating.css"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='app'>
+      <EmploymentSatisfactionForm/>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
